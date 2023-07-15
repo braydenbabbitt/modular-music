0bf0c299d50a66724bab8227a8cf5db19b3ad367
Brayden Babbitt <brayden@babbitts.net>
2023-07-15 14:46:12 -0600

    refactor: switch to zod for request validation

--- supabase/functions/execute-module/validation/validate-request.ts
+++ supabase/functions/execute-module/validation/validate-request.ts
@@ -1,74 +1,31 @@
-import { TsPrimitive } from './../types/generics.ts';
+import { z } from 'zod';
 
-type ValidationObject = {
-  key: string;
-  types: TsPrimitive[];
-};
+const schema = z.object({
+  moduleId: z
+    .string({
+      required_error: 'Missing required property: moduleId',
+      invalid_type_error: 'Expected property moduleId to be of type string',
+    })
+    .min(1, { message: 'Property moduleId cannot be an empty string' }),
+  scheduleId: z
+    .string({
+      invalid_type_error: 'Expected property scheduleId to be of type string',
+    })
+    .optional(),
+});
 
-const requestBodyValidation: ValidationObject[] = [
-  { key: 'moduleId', types: ['string'] },
-  { key: 'scheduleId', types: ['string', 'undefined'] },
-];
+export const validateRequest = async (request: Request) => {
+  const authHeader = request.headers.get('Authorization');
 
-type ExecuteModuleRequestBodyType = {
-  moduleId: string;
-  scheduleId?: string;
-};
-
-type ExecuteModuleRequestValidatedType = ExecuteModuleRequestBodyType & {
-  authHeader: string;
-};
-
-export const validateRequest = async (req: Request): Promise<ExecuteModuleRequestValidatedType> => {
-  try {
-    const parsedRequest = await req.json();
-    const authHeader = req.headers.get('Authorization');
-    if (!authHeader) {
-      throw new Error(
-        JSON.stringify({
-          message: 'Invalid request',
-          error: 'No authorization provided',
-        }),
-      );
-    }
-
-    if (typeof parsedRequest !== 'object') {
-      const errorMessage = `Expected body to be an object, but found ${typeof parsedRequest}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    const requestKeys = Object.keys(parsedRequest);
-    requestKeys.forEach((key) => {
-      const validationObj = requestBodyValidation.find((item) => item.key === key);
-      if (!validationObj) {
-        const errorMessage = `Expected to find parameter ${key} in request body, but found none`;
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      if (!validationObj.types.includes(typeof parsedRequest[key])) {
-        const errorMessage = `Expected parameter ${key} to be of type ${validationObj.types.join(
-          ' | ',
-        )}, but found ${typeof parsedRequest[key]}`;
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      return false;
-    });
-
-    const validatedRequestBody = {
-      moduleId: parsedRequest.moduleId,
-      scheduleId: parsedRequest.scheduleId ?? undefined,
-    } satisfies ExecuteModuleRequestBodyType;
-
-    return { ...validatedRequestBody, authHeader };
-  } catch (error) {
-    throw new Error(
-      JSON.stringify({
-        message: 'Error parsing request body.',
-        requestBody: req.body,
-        error,
-      }),
-    );
+  if (!authHeader) {
+    throw new Error('Invalid request: No authorization provided.');
   }
+
+  const validationResponse = schema.safeParse(await request.json());
+
+  if (!validationResponse.success) {
+    throw new Error('Invalid request: ' + JSON.stringify(validationResponse.error));
+  }
+
+  return { authHeader, ...validationResponse.data };
 };
--- supabase/functions/update-module-schedule/validation/validate-request.ts
+++ supabase/functions/update-module-schedule/validation/validate-request.ts
@@ -1,72 +1,30 @@
-import { TsPrimitiveName } from '../types/generics.ts';
+import { z } from 'zod';
 
-type ValidationObject = {
-  key: string;
-  types: TsPrimitiveName[];
-};
+const schema = z.object({
+  scheduleId: z
+    .string({
+      required_error: 'Missing required property: scheduleId',
+      invalid_type_error: 'Expected property scheduleId to be of type string',
+    })
+    .min(1, { message: 'Property scheduleId cannot be an empty string' }),
+  isNew: z.boolean({
+    required_error: 'Missing required property: isNew',
+    invalid_type_error: 'Expected property isNew to be of type boolean',
+  }),
+});
 
-const requestBodyValidation: ValidationObject[] = [
-  { key: 'scheduleId', types: ['string'] },
-  { key: 'isNew', types: ['boolean'] },
-];
+export const validateRequest = async (request: Request) => {
+  const authHeader = request.headers.get('Authorization');
 
-type UpdateModuleScheduleRequestBodyType = {
-  scheduleId: string;
-  isNew: boolean;
-};
-
-type UpdateModuleScheduleRequestValidatedType = UpdateModuleScheduleRequestBodyType & { authHeader: string };
-
-export const validateRequest = async (req: Request): Promise<UpdateModuleScheduleRequestValidatedType> => {
-  try {
-    const parsedRequest = await req.json();
-    const authHeader = req.headers.get('Authorization');
-    if (!authHeader) {
-      throw new Error(
-        JSON.stringify({
-          message: 'Invalid request',
-          error: 'No authorization provided',
-        }),
-      );
-    }
-
-    if (typeof parsedRequest !== 'object') {
-      const errorMessage = `Expected body to be an object, but found ${typeof parsedRequest}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    const requestKeys = Object.keys(parsedRequest);
-    requestKeys.forEach((key) => {
-      const validationObj = requestBodyValidation.find((item) => item.key === key);
-      if (!validationObj) {
-        const errorMessage = `Expected to find parameter ${key} in request body, but found none`;
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      if (!validationObj.types.includes(typeof parsedRequest[key])) {
-        const errorMessage = `Expected parameter ${key} to be of type ${validationObj.types.join(
-          ' | ',
-        )}, but found ${typeof parsedRequest[key]}`;
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-      return false;
-    });
-
-    const validatedRequestBody = {
-      scheduleId: parsedRequest.scheduleId,
-      isNew: parsedRequest.isNew,
-    } satisfies UpdateModuleScheduleRequestBodyType;
-
-    return { authHeader, ...validatedRequestBody };
-  } catch (error) {
-    throw new Error(
-      JSON.stringify({
-        message: 'Error parsing request body.',
-        requestBody: req.body,
-        error,
-      }),
-    );
+  if (!authHeader) {
+    throw new Error('Invalid request: No authorization provided.');
   }
+
+  const validationResponse = schema.safeParse(await request.json());
+
+  if (!validationResponse.success) {
+    throw new Error('Invalid request: ' + JSON.stringify(validationResponse.error));
+  }
+
+  return { authHeader, ...validationResponse.data };
 };
