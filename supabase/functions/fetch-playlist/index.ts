50de6ce1e8b1867027bccfe64434b407397d27d0
braydenbabbitt <brayden@babbitts.net>
2023-07-18 20:46:25 -0600

    feat: added playlist fetching logic

--- supabase/functions/fetch-playlist/index.ts
+++ supabase/functions/fetch-playlist/index.ts
@@ -1,8 +1,10 @@
+import { fetchPlaylistSnapshotId, fetchPlaylistTracks } from './spotify/fetch-playlist.ts';
 import { serve } from 'http-server';
 import { validateRequest } from './validation/validate-request.ts';
 import { createClient } from 'supabase-js';
 import { Database } from './types/database.ts';
-import { getSpotifyToken } from './spotify/get-token.ts';
+import { getSpotifyToken, refreshSpotifyToken } from './spotify/get-token.ts';
+import { attemptSpotifyApiRequest } from './spotify/token-helpers.ts';
 
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL');
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
@@ -37,15 +39,63 @@ serve(async (req) => {
 
   try {
     const { userId, playlistId } = await validateRequest(req);
-    const serviceRoleClient = createClient<Database>(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+    const publicSchemaClient = createClient<Database>(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+      auth: {
+        persistSession: false,
+      },
+    });
+    const spotifySchemaClient = createClient<Database, 'spotify'>(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
+      db: {
+        schema: 'spotify',
+      },
       auth: {
         persistSession: false,
       },
     });
 
-    const spotifyToken = await getSpotifyToken({ serviceRoleClient, userId });
+    const spotifyToken = await getSpotifyToken({ serviceRoleClient: publicSchemaClient, userId });
 
-    return new Response(JSON.stringify({ test: 'test' }));
+    const currentSnapshotId = await attemptSpotifyApiRequest(
+      (newToken?: string) => fetchPlaylistSnapshotId({ spotifyToken: newToken ?? spotifyToken, playlistId }),
+      () => refreshSpotifyToken({ serviceRoleClient: publicSchemaClient, userId }),
+    );
+
+    const savedPlaylist = await spotifySchemaClient.from('playlists').select().eq('id', playlistId).maybeSingle();
+    const shouldUseSavedPlaylist = savedPlaylist.data?.snapshot_id === currentSnapshotId;
+
+    if (shouldUseSavedPlaylist) {
+      return new Response(JSON.stringify(savedPlaylist.data?.track_ids ?? []), {
+        status: 200,
+        headers: {
+          ...CORS_HEADERS,
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const newPlaylistTracks = await attemptSpotifyApiRequest(
+      (newToken?: string) => fetchPlaylistTracks({ spotifyToken: newToken ?? spotifyToken, playlistId }),
+      () => refreshSpotifyToken({ serviceRoleClient: publicSchemaClient, userId }),
+    );
+    const newPlaylistTrackIds = newPlaylistTracks.map((track) => track.id);
+
+    spotifySchemaClient.from('playlists').upsert(
+      {
+        id: playlistId,
+        snapshot_id: currentSnapshotId,
+        track_ids: newPlaylistTrackIds,
+        last_fetched_at: new Date().toISOString(),
+      },
+      { onConflict: 'id' },
+    );
+
+    return new Response(JSON.stringify(newPlaylistTrackIds), {
+      status: 200,
+      headers: {
+        ...CORS_HEADERS,
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (err) {
     return new Response(err, {
       status: 400,
