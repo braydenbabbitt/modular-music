fa033e7373ad26357992a1989248b1e0906ae0ca
braydenbabbitt <brayden@babbitts.net>
2023-08-03 21:56:48 -0600

    fix(app): fixed issue with SpotifyTokenProvider when no user

--- src/services/spotify/spotify-token.provider.tsx
+++ src/services/spotify/spotify-token.provider.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useContext } from 'react';
 import { useSpotifyTokenQuery } from '../../hooks/use-spotify-token';
 import { PageContainer } from '../../components/containers/page-container.component';
 import { Center, Loader } from '@mantine/core';
+import { useAuth } from '../auth/auth.provider';
 
 type SpotifyTokenProviderProps = {
   children: ReactNode;
@@ -10,11 +11,14 @@ type SpotifyTokenProviderProps = {
 const SpotifyTokenContext = createContext<{ spotifyToken?: string } | undefined>(undefined);
 
 export const SpotifyTokenProvider = ({ children }: SpotifyTokenProviderProps) => {
+  const { user } = useAuth();
   const spotifyTokenQuery = useSpotifyTokenQuery();
+  const spotifyDataIsLoading = spotifyTokenQuery.isLoading || !spotifyTokenQuery.isFetched;
+  const showLoader = user && spotifyDataIsLoading;
 
   return (
     <SpotifyTokenContext.Provider value={{ spotifyToken: spotifyTokenQuery.data?.token }}>
-      {spotifyTokenQuery.isLoading || !spotifyTokenQuery.isFetched ? (
+      {showLoader ? (
         <PageContainer dontAdjustForHeader>
           <Center css={{ height: '100%' }}>
             <Loader />
